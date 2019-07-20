45091cdc07043cd6e5c4441800637d520b4d37c7
Vali Shah <vali.shaik@skyscanner.net>
2019-07-20 10:51:40 +0100

    Added --forceExit to jest

--- __tests__/fixture/spawn-sc.js
+++ __tests__/fixture/spawn-sc.js
@@ -2,11 +2,11 @@
 
 var sauceConnectLauncher = require("../../index");
 var options = JSON.parse(process.argv[2]);
-
 options.logger = console.log;
 
 sauceConnectLauncher(options, function (err)  {
   if (err) {
+    console.log(err);
     throw err;
   }
   process.exit(0);
